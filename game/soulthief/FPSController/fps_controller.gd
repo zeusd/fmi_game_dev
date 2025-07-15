a2de9ca706b8f8b76d0feb5fa059d5bcbfbfc3c5
zeusd <thegdimov@gmail.com>
2025-07-15 09:14:12 +0300

    "Add wallrun and walljump mechanics"

--- game/soulthief/FPSController/fps_controller.gd
+++ game/soulthief/FPSController/fps_controller.gd
@@ -11,23 +11,25 @@ extends CharacterBody3D
 @export var headbob_frequency := 2.4
 var headbob_time := 0.0
 
-var _cur_stick_look := Vector2.ZERO
+var cur_stick_look := Vector2.ZERO
 
-@export var jump_velocity := 5.0
+@export var jump_velocity := 13.0
 @export var auto_bhop = true
-@export var walk_speed := 7.0
-@export var sprint_speed := 8.5
-@export var ground_accel := 15.0
-@export var ground_decel := 10.0
-@export var ground_frict := 5.0
+@export var walk_speed := 4.3
+@export var sprint_speed := 7.7
 
-@export var air_cap := 0.85
-@export var air_accel := 800.0
-@export var air_move_speed := 500.0
+var wall_normal := Vector3.ZERO
+
+@export var frict := 6.0
+@export var accel := 100.0
+@export var stop_speed := 100.0
+@export var max_speed := 320.0
+@export var air_move_cap := 0.85
+@export var air_speed := 500.0
 
 var wish_dir := Vector3.ZERO
 
-func get_move_speed() -> float:
+func get_speed() -> float:
 	return sprint_speed if Input.is_action_pressed("sprint") else walk_speed
 
 func _ready():
@@ -50,13 +52,13 @@ func _unhandled_input(event: InputEvent) -> void:
 func _handle_stick_look_input(delta: float) -> void:
 	var target_look = Input.get_vector("look_left", "look_right", "look_up", "look_down").normalized()
 	
-	if target_look.length() < _cur_stick_look.length():
-		_cur_stick_look = target_look
+	if target_look.length() < cur_stick_look.length():
+		cur_stick_look = target_look
 	else:
-		_cur_stick_look = _cur_stick_look.lerp(target_look, (1 / stick_look_smoothing) * delta)
+		cur_stick_look = cur_stick_look.lerp(target_look, (1 / stick_look_smoothing) * delta)
 	
-	rotate_y(-_cur_stick_look.x * x_stick_sensitivity)
-	%Camera3D.rotate_x(_cur_stick_look.y * y_stick_sensitivity)
+	rotate_y(-cur_stick_look.x * x_stick_sensitivity)
+	%Camera3D.rotate_x(cur_stick_look.y * y_stick_sensitivity)
 	%Camera3D.rotation.x = clamp(%Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 	
 
@@ -70,32 +72,57 @@ func _headbob_effect(delta: float) -> void:
 
 func _process(delta: float) -> void:
 	_handle_stick_look_input(delta)
+	
+	var input_dir = Input.get_vector("left", "right", "up", "down").normalized()
+	
+	# mind player look direction for the negations
+	wish_dir = self.global_transform.basis * Vector3(-input_dir.x , 0.0, -input_dir.y)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
+			self.velocity.y += jump_velocity
+		
+		_handle_ground_physics(delta)
+	else :
+		_handle_air_physics(delta)
+	
+	move_and_slide()
 
-func clip_velocity(normal: Vector3, overbounce: float, delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	pass
+
+func _clip_velocity(normal: Vector3, overbounce: float, delta: float) -> void:
 	var backoff := self.velocity.dot(normal) * overbounce
 	
 	if backoff >= 0:
 		return
 	
 	self.velocity -= normal * backoff;
-	
-	# make sure
-	var adjust := self.velocity.dot(normal)
-	if adjust <= 0.0:
-		self.velocity -= normal * backoff;
 
 func _handle_ground_physics(delta: float) -> void:
-	var cur_speed = self.velocity.dot(wish_dir)
-	var add_speed = get_move_speed() - cur_speed
+	_friction(delta)
+	_accelerate(delta)
+	_headbob_effect(delta)
+
+func _handle_air_physics(delta: float) -> void:
+	var wish_veloc := wish_dir
+	var wish_speed := wish_dir.length()
 	
-	if add_speed > 0:
-		var accel_speed = ground_accel * delta * get_move_speed()
-		if accel_speed > add_speed:
-			accel_speed = add_speed
-		self.velocity += accel_speed * wish_dir
+	if wish_speed > max_speed:
+		wish_veloc *= (max_speed / wish_speed)
+		wish_speed = max_speed
 	
-	var control = ground_decel if self.velocity.length() < ground_decel else self.velocity.length()
-	var new_speed = self.velocity.length() - (control * ground_frict * delta)
+	_wall_run(delta)
+	
+	if is_on_floor():
+		_friction(delta)
+		_accelerate(delta)
+	else:
+		_air_accelerate(wish_veloc, delta)
+
+func _friction(delta) -> void:
+	var control = stop_speed if self.velocity.length() < stop_speed else self.velocity.length()
+	var new_speed = self.velocity.length() - (control * frict * delta)
 	
 	new_speed = max(0, new_speed)
 
@@ -103,39 +130,35 @@ func _handle_ground_physics(delta: float) -> void:
 		new_speed /= self.velocity.length()
 	
 	self.velocity *= new_speed
-	
-	_headbob_effect(delta)
 
-func _handle_air_physics(delta: float) -> void:
+func _accelerate(delta: float) -> void:
+	var cur_speed = self.velocity.dot(wish_dir)
+	var add_speed = get_speed() - cur_speed
+	
+	if add_speed > 0:
+		var accel_speed = accel * get_speed() * delta
+		if accel_speed > add_speed:
+			accel_speed = add_speed
+		self.velocity += accel_speed * wish_dir
+
+func _air_accelerate(wish_veloc: Vector3, delta: float) -> void:
 	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
 	
-	var cur_speed = self.velocity.dot(wish_dir)
-	var wish_speed = min((air_move_speed * wish_dir).length(), air_cap)
+	var wish_speed = min(air_move_cap, (air_speed * wish_dir).length())
+	var cur_speed = self.velocity.dot(wish_veloc)
 	var add_speed = wish_speed - cur_speed
 	
 	if add_speed > 0:
-		var accel_speed = air_accel * air_move_speed * delta
+		var accel_speed = accel * air_speed * delta
 		accel_speed = min(accel_speed, add_speed)
-		self.velocity += accel_speed * wish_dir
-	
-	if is_on_wall():
-		#self.motion_mode = CharacterBody3D.MOTION_MODE_FLOATING
-		self.velocity.y += 9 * delta
-		#clip_velocity(get_wall_normal(), 1, delta)
-	else:
-		self.motion_mode = CharacterBody3D.MOTION_MODE_GROUNDED
+		self.velocity += accel_speed * wish_veloc
 
-func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_vector("left", "right", "up", "down").normalized()
-	
-	# mind player look direction for the negations
-	wish_dir = self.global_transform.basis * Vector3(-input_dir.x , 0., -input_dir.y)
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
-			self.velocity.y = jump_velocity
-		_handle_ground_physics(delta)
-	else :
-		_handle_air_physics(delta)
-	
-	move_and_slide()
+func _wall_run(delta: float) -> void:
+	if is_on_wall() and Input.is_action_pressed("sprint"):
+		wall_normal = get_slide_collision(0).get_normal()
+		if Input.is_action_just_pressed("jump"):
+			self.velocity += frict * wall_normal
+			self.velocity.y = max_speed * delta
+		else:
+			self.velocity -= wall_normal
+			self.velocity.y += frict * delta
