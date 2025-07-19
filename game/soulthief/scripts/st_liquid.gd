f1f835595fe761b415da760a8eda2fc4f4c87997
zeusd <thegdimov@gmail.com>
2025-07-19 23:05:57 +0300

    Make distance fog when in liquid with appropriate tints

--- game/soulthief/scripts/st_liquid.gd
+++ game/soulthief/scripts/st_liquid.gd
@@ -1,8 +1,6 @@
 @tool
 extends Area3D
 
-const HM := SHADER_DIR + "CameraWaterOverlay.gdshader"
-
 const MESH_INST_SUFF := "mesh_instance"
 const SHADER_DIR := "res://shaders/"
 const TXR_SPC_DIR := "res://assets/textures/special/"
@@ -10,6 +8,41 @@ const TXR_SPC_DIR := "res://assets/textures/special/"
 const TXR_SUFF := ".tres"
 const SHADER_SUFF := ".gdshader"
 const SHD_MAT_SUFF := "_shader_material.tres"
+
+const TINT := "tint"
+const DENS := "dens"
+const ST_RIPPLE := "st_ripple_overlay"
+
+var env : WorldEnvironment = null
+const COL_MULT := 0.6
+const FOG_DIV := 300
+
+const WATER_DENS := 0.2
+const SLIME_DENS := 0.25
+const LAVA_DENS := 0.3
+
+const WATER_TINT := Vector4(0, 5, 10, 0)
+const LAVA_TINT := Vector4(10, 2, 0, 0)
+const SLIME_TINT := Vector4(0, 3, 0, 0)
+
+@export var col_rect : ColorRect
+@export var liquid_type : int
+@export var gamma : float
+
+const LIQ_TYPES := {
+	0: {
+		TINT: WATER_TINT,
+		DENS: WATER_DENS
+	},
+	1: {
+		TINT: SLIME_TINT,
+		DENS: SLIME_DENS
+	},
+	2: {
+		TINT: LAVA_TINT,
+		DENS: LAVA_DENS
+	}
+}
 
 const ST_LIQUID := "st_liquid"
 const ST_WATER0 := "st_water0"
@@ -24,14 +57,17 @@ const ST_LAVA1 := "st_lava1"
 @onready var slime1_shd_mat : ShaderMaterial = preload(SHADER_DIR + ST_SLIME1 + SHD_MAT_SUFF)
 @onready var lava1_shd_mat : ShaderMaterial = preload(SHADER_DIR + ST_LAVA1 + SHD_MAT_SUFF)
 
-var col_rect : ColorRect
-@onready var hmh : Shader = preload(HM)
+@onready var ripple_shader : Shader = preload(SHADER_DIR + ST_RIPPLE + SHADER_SUFF)
 
 #func _physics_process(delta: float) -> void:
 	#var player : Node3D = get_tree().get_nodes_in_group("player")[0]
 	#var head : Area3D = player.find_child("HeadArea")
 
 func _ready() -> void:
+	
+	# # #
+	# SCREEN RIPPLE EFFECT
+	# # #
 	col_rect = ColorRect.new()
 	col_rect.visible = false
 	col_rect.size = Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"), ProjectSettings.get_setting("display/window/size/viewport_height"))
@@ -39,8 +75,12 @@ func _ready() -> void:
 	col_rect.z_index = -10
 	col_rect.z_as_relative = false
 	col_rect.material = ShaderMaterial.new()
-	(col_rect.material as ShaderMaterial).shader = hmh
+	(col_rect.material as ShaderMaterial).shader = ripple_shader
+	(col_rect.material as ShaderMaterial).set_shader_parameter(TINT, (LIQ_TYPES[liquid_type][TINT]) * COL_MULT)
 	self.add_child(col_rect)
+	
+	env = self.get_tree().get_nodes_in_group("env")[0] as WorldEnvironment
+	
 	self.body_entered.connect(_on_body_entered)
 	self.body_exited.connect(_on_body_exited)
 	self.area_entered.connect(_on_area_entered)
@@ -54,22 +94,30 @@ func _on_body_exited(node: Node3D) -> void:
 
 func _on_area_entered(area: Area3D) -> void:
 	col_rect.visible = true
+	env.environment.volumetric_fog_density = LIQ_TYPES[liquid_type][DENS]
+	var col = LAVA_TINT / FOG_DIV
+	env.environment.volumetric_fog_albedo = Color(col.x, col.y, col.z, col.w)
 
 func _on_area_exited(area: Area3D) -> void:
 	col_rect.visible = false
+	env.environment.volumetric_fog_density = 0.0
+	env.environment.volumetric_fog_albedo = Color.WHITE
 
 func _func_godot_apply_properties(properties: Dictionary) -> void:
 	var shader_dict : Dictionary = {
-		ST_WATER0 : water0_shader,
-		ST_SLIME1 : slime1_shader,
-		ST_LAVA1 : lava1_shader
+		ST_WATER0: water0_shader,
+		ST_SLIME1: slime1_shader,
+		ST_LAVA1: lava1_shader
 	}
 	
 	var shd_mat_dict : Dictionary = {
-		ST_WATER0 : water0_shd_mat,
-		ST_SLIME1 : slime1_shd_mat,
-		ST_LAVA1 : lava1_shd_mat
+		ST_WATER0: water0_shd_mat,
+		ST_SLIME1: slime1_shd_mat,
+		ST_LAVA1: lava1_shd_mat
 	}
+	
+	liquid_type = properties.get("liquid_type")
+	gamma = properties.get("gamma")
 	
 	var mesh_inst_name := self.name.trim_suffix(ST_LIQUID) + MESH_INST_SUFF
 	var mesh_inst = self.find_child(mesh_inst_name) as MeshInstance3D
@@ -90,11 +138,14 @@ func _func_godot_apply_properties(properties: Dictionary) -> void:
 			break
 	
 	if res_name != null:
-		var shader = shader_dict.get(res_name)
-		var shd_mat = shd_mat_dict.get(res_name)
-
+		var shader : Shader = shader_dict.get(res_name)
+		var shd_mat : ShaderMaterial = shd_mat_dict.get(res_name)
+		
 		shd_mat.shader = shader
 		mesh_inst.mesh.surface_set_material(0, shd_mat)
+		var albedo = shd_mat.get_shader_parameter("albedo") as Color
+		albedo.a = gamma
+		shd_mat.set_shader_parameter("albedo", albedo)
 	# # #
 	else:
 	# # #
@@ -109,4 +160,3 @@ func _func_godot_apply_properties(properties: Dictionary) -> void:
 		material.uv1_scale = Vector3(0.3, 0.3, 0.3)
 		material.uv1_triplanar = true
 	# # #
-	
